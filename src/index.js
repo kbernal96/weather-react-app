c48342fac981230cff2d89bb1be2e920b6a11ca7
kbernal96 <60956525+kbernal96@users.noreply.github.com>
2022-01-25 21:23:42 -0600

    removed unused vars

--- src/index.js
+++ src/index.js
@@ -3,17 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import SearchEngine from "./SearchEngine";
-
-function WeatherApp() {
-  return (
-    <div className="App">
-      <h1>Weather App</h1>
-      <SearchEngine />
-    </div>
-  );
-}
-
 
 ReactDOM.render(
   <React.StrictMode>
