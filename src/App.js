c0cb061b8d8aafc8b721107d110177d5b4f29d5f
kbernal96 <60956525+kbernal96@users.noreply.github.com>
2022-01-25 21:35:32 -0600

    added footer and some css

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <SearchEngine />
+      <footer>
+        Coded by 
+        <a className="coder" href="https://github.com/kbernal96/weather-react-app" target="_blank" rel="noreferrer">Karla Bernal</a>
+      </footer>
     </div>
   );
 }
